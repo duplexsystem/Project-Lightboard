8b69cd73d5ec98508559923e5d97de7c80fc0d81
Zoe <duplexsys@protonmail.com>
2021-02-20 17:55:17 -0600

    Init

---
+++ Engine/Render/render.cpp
@@ -0,0 +1,5 @@
+//
+// Created by duplex on 2/20/21.
+//
+
+#include "render.h"
---
+++ Engine/Render/render.h
@@ -0,0 +1,14 @@
+//
+// Created by duplex on 2/20/21.
+//
+
+#ifndef PROJECT_LIGHTBOARD_RENDER_H
+#define PROJECT_LIGHTBOARD_RENDER_H
+
+
+class render {
+
+};
+
+
+#endif //PROJECT_LIGHTBOARD_RENDER_H
