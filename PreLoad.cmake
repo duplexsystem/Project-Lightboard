2bf65d756f4fd02975da552f7c23e5d7e474890c
Zoe <duplexsys@protonmail.com>
2021-02-28 20:59:42 -0600

    fixes

---
+++ PreLoad.cmake
@@ -0,0 +1,18 @@
+#Use Ninja if found
+find_program(NINJA_FOUND ninja)
+if(NINJA_FOUND)
+    SET (CMAKE_GENERATOR Ninja CACHE BOOL "" FORCE)
+    message(STATUS "Using Ninja Generator")
+endif()
+
+#Use Clang if available
+find_program(CLANG_FOUND clang)
+if(CLANG_FOUND)
+    SET (CMAKE_C_COMPILER "${CLANG_FOUND}" CACHE BOOL "" FORCE)
+    message(STATUS "Using Clang C Compiler")
+endif()
+find_program(CLANGPP_FOUND clang)
+if(CLANGPP_FOUND)
+    SET (CMAKE_CXX_COMPILER "${CLANGPP_FOUND}" CACHE BOOL "" FORCE)
+    message(STATUS "Using Clang++ C++ Compiler")
+endif()
